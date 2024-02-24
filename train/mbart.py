b7296e172cdf0e1594dd6b0c3145d2949b39a11f
sergiogg-ops <sergiogg1yo@gmail.com>
2024-02-24 11:24:17 +0100

    Adaptación a traducción

--- train/mbart.py
+++ train/mbart.py
@@ -1,15 +1,16 @@
 # Load model directly
-import transformers as trans
-import torch
-import evaluate
 from argparse import ArgumentParser
+
+import evaluate
+import torch
+import transformers as trans
 
 parser = ArgumentParser('Script que automatiza el fine-tunning de un modelo MBART.')
 parser.add_argument('-args','--argument_file',help='Archivo yaml del que se toman los argumentos del entrenamiento.')
-parser.add_argument('-src_tr','--source_train',default='EuTrans/train/training.en',help='Archivo con el conjunto de entrenamiento en lengua origen.')
-parser.add_argument('-src_ev','--source_evaluation',default='EuTrans/dev/development.en',help='Archivo con el conjunto de evaluacion en lengua origen.')
-parser.add_argument('-tgt_tr','--target_train',default='EuTrans/train/training.es',help='Archivo con el conjunto de entrenamiento en lengua destino.')
-parser.add_argument('-tgt_ev','--target_evaluation',default='EuTrans/dev/development.es',help='Archivo con el conjunto de evaluacion en lengua destino.')
+parser.add_argument('-src_tr','--source_train',default='EuTrans/train/training.es',help='Archivo con el conjunto de entrenamiento en lengua origen.')
+parser.add_argument('-src_ev','--source_evaluation',default='EuTrans/dev/development.es',help='Archivo con el conjunto de evaluacion en lengua origen.')
+parser.add_argument('-tgt_tr','--target_train',default='EuTrans/train/training.en',help='Archivo con el conjunto de entrenamiento en lengua destino.')
+parser.add_argument('-tgt_ev','--target_evaluation',default='EuTrans/dev/development.en',help='Archivo con el conjunto de evaluacion en lengua destino.')
 parser.add_argument('-out_dir','--output_dir',default='mbart',help='Directorio en que se escriben las predicciones del modelo y checkpoints.')
 parser.add_argument('-a_out_dir','--overwrite_output_dir',action='store_true',default=False,help='Indica si se sobreescribe el directorio de salida.')
 parser.add_argument('-bs','--batch_size',default=32,type=int,help='Tamaño de batch.')
@@ -31,7 +32,7 @@ parser.add_argument('-warm_start','--resume_from_checkpoint',action='store_true'
 args = parser.parse_args()
 
 if args.max_steps:
-    training_args = trans.TrainingArguments(output_dir=args.output_dir,overwrite_output_dir=args.overwrite_output_dir,
+    training_args = trans.Seq2SeqTrainingArguments(output_dir=args.output_dir,overwrite_output_dir=args.overwrite_output_dir,
                                             per_device_train_batch_size=args.batch_size, per_device_eval_batch_size=args.batch_size,
                                             evaluation_strategy=args.eval_strategy,learning_rate=args.learning_rate,
                                             weight_decay=args.weight_decay,max_steps=args.max_steps,
@@ -43,7 +44,7 @@ if args.max_steps:
                                             optim=args.optim,
                                             resume_from_checkpoint=args.resume_from_checkpoint)
 else:
-    training_args = trans.TrainingArguments(output_dir=args.output_dir,overwrite_output_dir=args.overwrite_output_dir,
+    training_args = trans.Seq2SeqTrainingArguments(output_dir=args.output_dir,overwrite_output_dir=args.overwrite_output_dir,
                                             per_device_train_batch_size=args.batch_size, per_device_eval_batch_size=args.batch_size,
                                             evaluation_strategy=args.eval_strategy,learning_rate=args.learning_rate,
                                             weight_decay=args.weight_decay,num_train_epochs=args.epochs,
@@ -58,8 +59,7 @@ else:
 tokenizer = trans.AutoTokenizer.from_pretrained("facebook/mbart-large-50-many-to-one-mmt")
 model = trans.AutoModelForSeq2SeqLM.from_pretrained("facebook/mbart-large-50-many-to-one-mmt")
 
-def tokenize_function(example):
-    return tokenizer(example, padding="max_length", truncation=True)
+data_collator = trans.DataCollatorForSeq2Seq(tokenizer, model=model)
 
 class Eutrans(torch.utils.data.Dataset):
     def __init__(self, source_file, target_file):
@@ -74,25 +74,40 @@ class Eutrans(torch.utils.data.Dataset):
         return len(self.source)
     
     def __getitem__(self, idx):
-        return {'id':idx,
-                'translation':{self.src_lang: tokenize_function(self.source[idx]),
-                self.tgt_lang: tokenize_function(self.target[idx])}}
+        input = tokenizer(self.source[idx],text_target=self.target[idx])
+        return input
 
 train_data = Eutrans(args.source_train,args.target_train)
 eval_data = Eutrans(args.source_evaluation,args.target_evaluation)
 
-metric = evaluate.load('bleu')
-def compute_metrics(eval_pred):
-    predictions, labels = eval_pred
-    #predictions = torch.argmax(logits, axis=-1)
-    return metric.compute(predictions=predictions, references=labels)
+metric = evaluate.load('sacrebleu')
 
-trainer = trans.Trainer(
-    model=model,
-    args=training_args,
+
+def compute_metrics(eval_preds):
+    preds, labels = eval_preds
+    # In case the model returns more than the prediction logits
+    if isinstance(preds, tuple):
+        preds = preds[0]
+
+    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+
+    # Replace -100s in the labels as we can't decode them
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    # Some simple post-processing
+    decoded_preds = [pred.strip() for pred in decoded_preds]
+    decoded_labels = [[label.strip()] for label in decoded_labels]
+
+    result = metric.compute(predictions=decoded_preds, references=decoded_labels)
+    return {"bleu": result["score"]}
+
+trainer = trans.Seq2SeqTrainer(
+    model,
+    training_args,
     train_dataset=train_data,
     eval_dataset=eval_data,
+    data_collator=data_collator,
+    tokenizer=tokenizer,
     compute_metrics=compute_metrics,
 )
-
-trainer.train()
